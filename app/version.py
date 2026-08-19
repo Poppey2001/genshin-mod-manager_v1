edb823030acb11613145aea85caef5cfe430d5c4
Poppey2001 <poppe838@gmail.com>
2026-08-19 11:49:34 +0200

    test

--- app/version.py
+++ app/version.py
@@ -6,10 +6,10 @@ APP_NAME = "Genshin Mod Manager"
 # Interne Version für Vergleiche.
 # PEP-440-kompatibel:
 # 0.4.0a1 < 0.4.0a2 < 0.4.0b1 < 0.4.0
-APP_VERSION = "0.5.8b5"
+APP_VERSION = "0.5.8b6"
 
 # Schön dargestellte Version für die Oberfläche.
-APP_VERSION_DISPLAY = "0.5.8 Beta5"
+APP_VERSION_DISPLAY = "0.5.8 Beta6"
 
 
 __version__ = APP_VERSION
