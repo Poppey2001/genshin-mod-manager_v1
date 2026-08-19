39aa60d81c788101f8f630a791535db00479ad4b
Poppey2001 <poppe838@gmail.com>
2026-08-19 10:24:09 +0200

    Test Auto Updater

--- app/version.py
+++ app/version.py
@@ -6,10 +6,10 @@ APP_NAME = "Genshin Mod Manager"
 # Interne Version für Vergleiche.
 # PEP-440-kompatibel:
 # 0.4.0a1 < 0.4.0a2 < 0.4.0b1 < 0.4.0
-APP_VERSION = "0.5.7b3"
+APP_VERSION = "0.5.8b1"
 
 # Schön dargestellte Version für die Oberfläche.
-APP_VERSION_DISPLAY = "0.5.7 Beta3"
+APP_VERSION_DISPLAY = "0.5.8 Beta1"
 
 
 __version__ = APP_VERSION
